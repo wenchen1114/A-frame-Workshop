0e2358f3ee5ce970124b117e04a786ed46c1c43a
wenchen1114 <kwc354@nyu.edu>
2021-03-21 00:02:56 -0400

    writing-up

--- site.js
+++ site.js
@@ -1,3 +1,18 @@
 let star = document.getElementById("star");
-let button = document.getElementById("button");
 
+AFRAME.registerComponent('turngreen', {
+    init: function () {
+      this.el.addEventListener('mouseenter', function(e){
+        this.setAttribute('color', '#00FF00')
+        star.setAttribute('animation', 'property:rotation; to: 0 180 0 dur: 2000; easing: linear; loop: false');
+      })
+    }
+  })
+
+  AFRAME.registerComponent('turnred', {
+    init: function () {
+      this.el.addEventListener('mouseleave', function(e){
+        this.setAttribute('color', '#FF0000')
+      })
+    }
+  })
